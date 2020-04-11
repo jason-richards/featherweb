59f94180c14d53e1527a4ab3c8c4ecc0b61772b2
jason-richards <jason.richards.eng@gmail.com>
2020-04-10 20:19:06 -0700

    Update __init__.py
    Give handler callbacks more information about the request being made;  support url parameters.

--- featherweb/__init__.py
+++ featherweb/__init__.py
@@ -1,6 +1,7 @@
 import usocket as socket
 import uselect as select
 
+
 class FeatherWeb(object):
     m_Socket = None
     m_Routes = []
@@ -11,7 +12,8 @@ class FeatherWeb(object):
         self.m_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.m_Socket.bind(address)
         self.m_Socket.listen(maxQ)
-                
+        self.m_Socket.setblocking(True)
+
 
     def __del__(self):
         self.m_Socket.close()
@@ -43,39 +45,38 @@ class FeatherWeb(object):
                     poller.unregister(self.m_Socket)
                     raise Exception ("POLLHUP/POLLERR")
 
-                if fd is not self.m_Socket or not event & select.POLLIN:
+                if id(fd) is not id(self.m_Socket) or not event & select.POLLIN:
                     continue
 
-
                 client, address = self.m_Socket.accept()
+
                 try:
                     f = client.makefile('rwb', 0)
-                    request = f.readline()
-                    method, path, proto = request.decode().split()
+
+                    response = HTTPRequest(client, f.readline())
 
                     # This may be dangerous for the ESP8266.  Request headers may be extensively large - a simple
                     # Request with lots of HTTP headers could cause OOM crash. Request headers may be 8-16KB!
-                    headers = {}
                     while True:
                         line = f.readline()
                         if not line or line == b'\r\n':
                             break
                         k, v = line.split(b":", 1)
-                        headers[k] = v.strip()
+                        response.headers[k] = v.strip()
 
                     found = False
                     for e in self.m_Routes:
                         pattern = e[0]
                         handler = e[1]
 
-                        if path == pattern:
+                        if response.path.split('?', 1)[0] == pattern:
                             found = True
                             break
 
                     if not found:
                         raise
 
-                    handler(client)
+                    handler(response)
 
                 except Exception as e:
                     client.sendall('HTTP/1.0 404 NA\r\n\r\n')
@@ -86,39 +87,49 @@ class FeatherWeb(object):
         poller.unregister(self.m_Socket)
 
         
-class HTTPResponse():
+class HTTPRequest():
 
-    def __init__(self, client, content_type="text/html; charset=utf-8", status="200", headers=None):
+    def __init__(self, client, request, content_type="text/html; charset=utf-8", status="200", headers={}):
         """ Utility object for HTTP request responses. """
         self.client = client
-        client.sendall("HTTP/1.0 %s NA\r\n" % status)
-        client.sendall("Content-Type: ")
-        client.sendall(content_type)
-        if not headers:
-            client.sendall("\r\n\r\n")
+        self.method, self.path, self.proto = request.decode().split()
+        self.content_type = content_type
+        self.status = status
+        self.headers = headers
+
+
+    def __headers(self):
+        self.client.sendall("HTTP/1.0 %s NA\r\n" % self.status)
+        self.client.sendall("Content-Type: ")
+        self.client.sendall(self.content_type)
+        if not self.headers:
+            self.client.sendall("\r\n\r\n")
         else:
-            client.sendall("\r\n")
-            if isinstance(headers, bytes) or isinstance(headers, str):
-                client.sendall(headers)
+            self.client.sendall("\r\n")
+            if isinstance(self.headers, bytes) or isinstance(self.headers, str):
+                self.client.sendall(self.headers)
             else:
-                for k, v in headers.items():
-                    client.sendall(k)
-                    client.sendall(": ")
-                    client.sendall(v)
-                    client.sendall("\r\n")
-            client.sendall("\r\n")
+                for k, v in self.headers.items():
+                    self.client.sendall(k)
+                    self.client.sendall(": ")
+                    self.client.sendall(v)
+                    self.client.sendall("\r\n")
+            self.client.sendall("\r\n")
 
 
-    def sendtext(self, response):
+    def send(self, response):
         """ Send a textual response. """
+        self.__headers()
         self.client.sendall(response)
 
 
     def sendfile(self, filename, chunksize=128):
         """ Send a file in response, one chunk at a time.  Caller handles exceptions. """
         with open(filename, 'rb') as f:
+            self.__headers()
             while True:
                 data = f.read(chunksize)
                 if not data:
                     break
                 self.client.sendall(data)
+
